30cf41ca2107af8f4a9653c0732c99b1fcf1f1af
Zitai Huang <mywpiedu@gmail.com>
2021-08-12 11:34:01 -0500

    animation`

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-//eventlisner for messageBars
 let messageBar = document.querySelector('#messageBar')
 let levelButtons = document.querySelector('#levels')
+
+//A liitle bit of animate
+let x = 1
+let y = 1
+if (messageBar.innerHTML == 'Press here to start the game!') {
+  setInterval(() => {
+    if (x >= -1) {
+      x -= 0.02
+    }
+    if (x <= -1) {
+      x = 1
+    }
+
+    messageBar.style.color = `rgba(0,0,0,${Math.abs(x)})`
+  }, 30)
+}
+
+//eventlisner for messageBars
+
 messageBar.addEventListener('click', (e) => {
   if (messageBar.innerHTML == 'Press here to start the game!') {
+    clearInterval(`1`)
+    messageBar.style.color = `rgba(0,0,0,1)`
     messageBar.innerHTML = 'Choos a difficulty:'
     levelButtons.style.display = 'flex'
   }
 })
-
-//A liitle bit of animate
-let x =1
-let y =`rgba(0,0,0,${x})`
-if (messageBar.innerHTML='Press here to start the game!'){
-    if (x ==1)
-    let blinking = ()=>{
-        setInterval(() => {
-            
-            x= x - 0.05
-
-            messageBar.style.color ==y
-        }, 500);
-
-    }
-}
